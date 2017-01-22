7228fcdef34c1c11b579ede5f520dfd017d02704
Bryce Willey <Bryce.Steven.Willey@gmail.com>
2017-01-21 23:26:38 -0600

    Added follow and pan to character.

--- Classes/Viewpoint.cpp
+++ Classes/Viewpoint.cpp
@@ -10,8 +10,8 @@
 
 using namespace JTTW;
 
-Viewpoint::Viewpoint(cocos2d::Size dims, double metersPerPixel) :
-        _screenDims(dims), _metersPerPixel(metersPerPixel) {}
+Viewpoint::Viewpoint(cocos2d::Size dims, double metersPerPixel, cocos2d::Layer *level) :
+_screenDims(dims), _metersPerPixel(metersPerPixel), _level(level) {}
 
 int Viewpoint::metersToPixels(double meters) const {
     return meters / _metersPerPixel;
@@ -19,4 +19,18 @@ int Viewpoint::metersToPixels(double meters) const {
 
 double Viewpoint::pixelsToMeters(int pixels) const {
     return pixels * _metersPerPixel;
+}
+
+void Viewpoint::panToCharacter(Character* player) {
+    // So, we know the level location, the width of the screen, and the player location.
+    // When level->position.x == 0, then the center of the screen is at width/2.0.
+    // width/2.0 = level->position.x + player->ani->position.x;
+    float newLevelX = _screenDims.width/2.0 - player->ani->getPosition().x;
+    auto action = cocos2d::MoveTo::create(1.0, cocos2d::Vec2(newLevelX, _level->getPosition().y));
+    _level->runAction(action);
+}
+
+void Viewpoint::followCharacter(Character *player) {
+    float newLevelX = _screenDims.width / 2.0 - player->ani->getPosition().x;
+    _level->setPosition(cocos2d::Vec2(newLevelX, _level->getPosition().y));
 }
--- Classes/Viewpoint.hpp
+++ Classes/Viewpoint.hpp
@@ -10,11 +10,16 @@
 #define Viewpoint_hpp
 
 #include <stdio.h>
+#include "Character.h"
 
 namespace JTTW {
 class Viewpoint {
 public:
-    Viewpoint(cocos2d::Size screenDims, double metersPerPixel);
+    Viewpoint(cocos2d::Size screenDims, double metersPerPixel, cocos2d::Layer *level);
+    
+    void panToCharacter(Character *player);
+    void followCharacter(Character *player);
+    
     
     int metersToPixels(double meters) const;
     double pixelsToMeters(int pixels) const;
@@ -22,6 +27,8 @@ public:
 private:
     cocos2d::Size _screenDims; // The dimensions of the screen at the time.
     double _metersPerPixel; // The dimensions of the portion of the scene shown on screen in meters
+    
+    cocos2d::Layer *_level; // get from the level layer->getPosition.
 };
 };
 
