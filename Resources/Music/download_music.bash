f0d1f1584563187a91980e48fd104c9aa10ad99d
Bryce Willey <Bryce.Steven.Willey@gmail.com>
2017-04-19 22:25:12 -0500

    Finished download music script.

--- Resources/Music/download_music.bash
+++ Resources/Music/download_music.bash
@@ -1,21 +1,32 @@
+#!/bin/bash
 # To Respect the artists whose music we've been allowed to use under their permissions, we
 # cannot include copies of their music in this public avaliable repository.
 
 # Run this script to download a copy directly from their websites to support the artists.
 
-# TODO: Credits
-# TODO: Greed
-# TODO: Truce
-# TODO: Flight
-# TODO: Menu Screen
-# TODO: Breakout
-wget http://agnello-pecora.chu.jp/CAMeLIA/mus/h10_-_-_Rin_-.mp3
-mv h10_-_-_Rin_-.mp3 Paradise.mp3
-# TODO: Harmony
-# TODO: Bodhi
-# TODO: Golden Temple
+wget http://timelessberry.com/material/item/sao1/chapter0-pr.mp3
+mv chapter0-pr.mp3 Credits.mp3
+wget http://hagall.hacca.jp/music/mp3/rai_bokyaku.mp3
+mv rai_bokyaku.mp3 Greed.mp3
+wget http://ontama-m.com/midi/data/mp3_file/original/chinkou.mp3
+mv chinkou.mp3 Truce.mp3
+wget https://dova4.heteml.jp/dova/mp3/109.mp3
+mv 109.mp3 Flight.mp3
+wget http://ontama-m.com/midi/data/mp3_file/original/amami.mp3
+mv amami.mp3 MenuScreen.mp3
+wget http://ontama-m.com/midi/data/mp3_file/original/fortune.mp3
+mv fortune.mp3 Breakout.mp3
+wget http://agnello-pecora.chu.jp/CAMeLIA/mus/h10.mp3
+mv h10.mp3 Paradise.mp3
+wget http://hagall.hacca.jp/music/mp3/rai_tabi.mp3
+mv rai_tabi.mp3
+wget http://agnello-pecora.chu.jp/CAMeLIA/mus/s13.mp3
+mv s13.mp3 Bodhi.mp3
+wget http://agnello-pecora.chu.jp/CAMeLIA/mus/1a.mp3
+mv 1a.mp3 Golden\ Temple.mp3
 wget http://agnello-pecora.chu.jp/CAMeLIA/mus/a8.mp3
 mv a8.mp3 Freedom.mp3
-# TODO: TrucePt2
-# TODO: Secret Garden
-wget    tw043_-_storage_of_forest.mp3
+wget http://agnello-pecora.chu.jp/CAMeLIA/mus/s17.mp3
+mv s17.mp3 TrucePt2.mp3
+wget http://musmus.main.jp/music/tw043.mp3
+mv tw043.mp3 Secret\ Garden.mp3
